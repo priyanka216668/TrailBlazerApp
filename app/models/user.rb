67201b61b48f74b5f658306313c0601f6b007a60
priyanka216668 <priyanka@joshsoftware.com>
2018-07-19 19:17:54 +0530

    [Added] devise to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
-	has_many :post
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
+  has_many :posts
+  belongs_to :role
+
 end
