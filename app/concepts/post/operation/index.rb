db41d4e424b28dab158c3359499dcf26fc5e0580
priyanka216668 <priyanka@joshsoftware.com>
2018-07-20 16:07:59 +0530

    Received current_user in post index operation

--- app/concepts/post/operation/index.rb
+++ app/concepts/post/operation/index.rb
@@ -1,11 +1,11 @@
 class Post::Index < Trailblazer::Operation
-	step :display
+  step :display
 
-	def display(options, model, **)
-		#byebug
-		if options["params"]["format"]!= nil
-		  user_id = options["params"]["format"].to_i
-		  options["model"] = User.find(user_id).posts
-		end
-	end
+  def display(options, model, **)
+    if options["params"]["format"]!= nil
+      user_id = options["params"]["format"].to_i
+      options["model"] = User.find(user_id).posts
+      options["current_user"] = options["params"]["current_user"]
+    end
+  end
 end
