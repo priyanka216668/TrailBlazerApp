873edc217c6a2459e1e5021fd0a35d78a49a5b5c
priyanka216668 <priyanka@joshsoftware.com>
2018-07-23 17:19:01 +0530

    Added validation for title in Post contract

--- app/concepts/post/contract/create.rb
+++ app/concepts/post/contract/create.rb
@@ -1,6 +1,8 @@
 module Post::Contract
   class Create < Reform::Form
     property :content
+    property :title
     validates :content, presence: true
+    validates :title, presence: true
   end
 end
