f4336112a952d28a8de6de1d771f9a629fc3c624
priyanka216668 <priyanka@joshsoftware.com>
2018-07-23 17:27:54 +0530

    Changed schema of User, Post

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_19_053923) do
+ActiveRecord::Schema.define(version: 2018_07_23_100855) do
 
   create_table "posts", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.string "title"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
@@ -30,7 +31,6 @@ ActiveRecord::Schema.define(version: 2018_07_19_053923) do
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "name"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -42,6 +42,10 @@ ActiveRecord::Schema.define(version: 2018_07_19_053923) do
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
     t.integer "role_id"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "gender"
+    t.string "phone_number"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["role_id"], name: "index_users_on_role_id"
