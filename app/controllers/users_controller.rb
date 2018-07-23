92ea4633192ced2fafd0ea93f9aef3ccd82590af
priyanka216668 <priyanka@joshsoftware.com>
2018-07-23 15:01:10 +0530

    render layout in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 	def index
-		run User::Index
-		render cell(User::Cell::Index, result["model"])
+  	run User::Index
+  	render cell(User::Cell::Index, result["model"], layout: Gemgem::Cell::Layout)
 	end
 end
