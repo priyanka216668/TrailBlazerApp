fd0d3fb33c64fc3b13cef06fd09c100b54ec90b4
priyanka216668 <priyanka@joshsoftware.com>
2018-07-23 17:28:23 +0530

    Added seed file to generate data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+#Uncomment below lines to generate data
+
+# role = Role.new(name:'normal_user', priority: 2)
+# role.save!
+#require 'csv'
+
+# csv_text = File.read(Rails.root.join('lib', 'seed', 'user_data.csv'))
+# csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+# csv.each do |row|
+#    u = User.new
+#   u.first_name = row['first_name']
+#   u.last_name = row['last_name']
+#   u.email = row['email']
+#   u.gender= row['gender']
+#   u.phone_number= row['phone_number']
+#   u.password = row['password']
+#   u.role_id = row['role_id'].to_i
+#   u.save
+# end
+# csv_text = File.read(Rails.root.join('lib', 'seed', 'post_data.csv'))
+# csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+# csv.each do |row|
+#   p = Post.new
+#   p.title = row['title']
+#   p.content= row['content']
+#   p.user_id = row['user_id'].to_i
+#   p.save
+# end
+
