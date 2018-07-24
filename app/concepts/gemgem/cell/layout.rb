beb220e35e18af248cf1b76675988fc4bcb725ce
priyanka216668 <priyanka@joshsoftware.com>
2018-07-24 15:18:26 +0530

    [Added] navbar to layout.slim

--- app/concepts/gemgem/cell/layout.rb
+++ app/concepts/gemgem/cell/layout.rb
@@ -1,4 +1,12 @@
 module Gemgem::Cell
   class Layout < Trailblazer::Cell
+    def user_signed_in?
+      return false unless options[:context][:controller].current_devise_user != nil
+      true
+    end
+
+    def current_user
+      options[:context][:controller].current_devise_user.first_name
+    end
   end
 end
