be877188a5db8843574f76729ae526505e9c2cbb
priyanka216668 <priyanka@joshsoftware.com>
2018-07-26 18:35:59 +0530

    Nested route created for user,post

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   root 'users#index'
   devise_for :users, as: 'devise'
   resource :home
-  # resources :users do
-  # 	resources :posts
-  # end
+  resources :users do
+    resources :posts
+  end
 
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
