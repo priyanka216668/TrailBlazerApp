65d978b94b4baf8777a42f7315f2dd130d88e661
priyanka216668 <priyanka@joshsoftware.com>
2018-07-26 18:23:49 +0530

    Content method created in item.rb

--- app/concepts/post/cell/item.rb
+++ app/concepts/post/cell/item.rb
@@ -1,7 +1,10 @@
 module Post::Cell
   class Item < Trailblazer::Cell
     def title
-      link_to model.title, post_path(model.id) unless model == nil
+      model.title unless model == nil
+    end
+    def content
+      model.content unless model == nil
     end
     def user_signed_in?
       return false unless options[:context][:controller].current_devise_user != nil
