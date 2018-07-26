a4c756a2b1b3374463e8fa7f806ffca3641cc064
priyanka216668 <priyanka@joshsoftware.com>
2018-07-26 18:26:34 +0530

    Receiving user id in user_id param in index.rb

--- app/concepts/post/operation/index.rb
+++ app/concepts/post/operation/index.rb
@@ -2,8 +2,8 @@ class Post::Index < Trailblazer::Operation
   step :display
 
   def display(options, model, **)
-    if options["params"]["format"]!= nil
-      user_id = options["params"]["format"].to_i
+    if options["params"]["user_id"]!= nil
+      user_id = options["params"]["user_id"].to_i
       options["model"] = User.find(user_id).posts
       options["current_user"] = options["params"]["current_user"]
     end
