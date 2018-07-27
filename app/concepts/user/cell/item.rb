034917dfdc5fa2ae60fcac77c09b834e3c29d0ab
priyanka216668 <priyanka@joshsoftware.com>
2018-07-27 15:58:56 +0530

    Removed link for user name in item.rb

--- app/concepts/user/cell/item.rb
+++ app/concepts/user/cell/item.rb
@@ -1,15 +1,11 @@
 module User::Cell
   class Item < Trailblazer::Cell
     def first_name
-      link_to model.first_name, model unless model == nil
+      model.first_name unless model == nil
     end
 
     def post_count
       link_to model.posts.count, user_posts_path(model.id)
-    end
-
-    def log_in
-      link_to "Login", devise_user_session_path
     end
   end
 end
