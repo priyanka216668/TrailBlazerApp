b0fb663dc3bb47d4252a1fdd1109411d4a363cad
priyanka216668 <priyanka@joshsoftware.com>
2018-08-29 10:43:18 +0530

    Added path for coffee script in application.rb

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,6 @@ module Demo
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.cells.with_assets = ["post/cell/item"]
   end
 end
